d35d8e46a88168477cfb6f4c8c0938ee980115e7
codyzzd <codyzzd@gmail.com>
2023-09-09 20:27:07 -0300

    favicon ajust

--- include/_head.php
+++ include/_head.php
@@ -1,8 +1,34 @@
-<link rel="stylesheet" href="../css.css">
-<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
+<link rel="stylesheet"
+      href="../css.css">
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css"
+      rel="stylesheet"
+      integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9"
+      crossorigin="anonymous">
 <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.3.0/js/bootstrap.min.js"></script>
 <!--<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">-->
 
-<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css">
+<link rel="stylesheet"
+      href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css">
 <!-- notyf -->
-<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/notyf@3/notyf.min.css">
+<link rel="stylesheet"
+      href="https://cdn.jsdelivr.net/npm/notyf@3/notyf.min.css">
+
+<!-- Favicon para navegadores modernos -->
+<link rel="icon"
+      type="image/png"
+      href="/favicon.png"
+      sizes="32x32">
+<link rel="icon"
+      type="image/png"
+      href="/favicon.png"
+      sizes="16x16">
+
+<!-- Favicon para dispositivos Apple -->
+<link rel="apple-touch-icon"
+      sizes="180x180"
+      href="/favicon.png">
+
+<!-- Favicon genérico -->
+<!--<link rel="shortcut icon"
+      href="/favicon.ico"
+      type="image/x-icon">-->
