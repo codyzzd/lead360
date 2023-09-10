08f864a4b75f4fefec0226ffe5df2f3b0e9b538f
codyzzd <codyzzd@gmail.com>
2023-09-10 14:58:54 -0300

    ajuste no controle do tempo da sessão

--- include/_a_check_login.php
+++ include/_a_check_login.php
@@ -1,8 +1,15 @@
 <?php
 session_start();
 
-if (!isset($_SESSION['user_email'])) {
+$time_now = time();
+$session_timeout = 60 * 60 * 12; // 12 horas em segundos
+$time_last_activity = session_last_activity();
+
+if ($time_now - $time_last_activity > $session_timeout) {
+  // O usuário está inativo, então deslogá-lo.
   session_destroy();
   header("Location: ../admin/login.php");
   exit();
 }
+
+// O usuário está ativo, então continue.
