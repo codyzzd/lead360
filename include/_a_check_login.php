cb95bd944b1c772f901ec3f50fdd5ce2d82b9fbb
codyzzd <codyzzd@gmail.com>
2023-09-10 15:01:14 -0300

    ajuste na sessão

--- include/_a_check_login.php
+++ include/_a_check_login.php
@@ -1,21 +1,10 @@
 <?php
-/*
 session_start();
 
 if (!isset($_SESSION['user_email'])) {
   session_destroy();
   header("Location: ../admin/login.php");
   exit();
-}*/
-
-
-$time_now = time();
-$session_timeout = 60 * 60 * 12; // 12 horas em segundos
-$time_last_activity = session_last_activity();
-
-if ($time_now - $time_last_activity > $session_timeout) {
-  // O usuário está inativo, então deslogá-lo.
-  session_destroy();
-  header("Location: ../admin/login.php");
-  exit();
 }
+
+
