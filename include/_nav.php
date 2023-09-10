9d0fb76e32f8a3f09f6bdd98f469993d4a4db2c4
codyzzd <codyzzd@gmail.com>
2023-09-10 01:54:47 -0300

    navbar fix image

--- include/_nav.php
+++ include/_nav.php
@@ -5,7 +5,7 @@
   <div class="container-fluid">
     <a class="navbar-brand"
        href="#">
-      <img src="/logo_nav.png"
+      <img src="/logonav.png"
            alt="LiderScan"
            height="32">
     </a>
