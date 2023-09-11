d8d034039b362dd2abf0ca1fa3ac7e4d77583e77
codyzzd <codyzzd@gmail.com>
2023-09-11 17:40:36 -0300

    ajuste brevo tag

--- teste.php
+++ teste.php
@@ -48,6 +48,33 @@ if ($result->num_rows > 0) {
     <?php include 'include/_head.php' ?>
   </head>
 
+  <script type="text/javascript">
+    (function () {
+      window.sib = {
+        equeue: [],
+        client_key: "ut2sod5zdi6nrwd2to8r6zr6"
+      };
+      /* OPTIONAL: email for identify request*/
+      // window.sib.email_id = 'example@domain.com';
+      window.sendinblue = {};
+      for (var j = ['track', 'identify', 'trackLink', 'page'], i = 0; i < j.length; i++) {
+        (function (k) {
+          window.sendinblue[k] = function () {
+            var arg = Array.prototype.slice.call(arguments);
+            (window.sib[k] || function () {
+              var t = {};
+              t[k] = arg;
+              window.sib.equeue.push(t);
+            })(arg[0], arg[1], arg[2], arg[3]);
+          };
+        })(j[i]);
+      }
+      var n = document.createElement("script"),
+        i = document.getElementsByTagName("script")[0];
+      n.type = "text/javascript", n.id = "sendinblue-js", n.async = !0, n.src = "https://sibautomation.com/sa.js?key=" + window.sib.client_key, i.parentNode.insertBefore(n, i), window.sendinblue.page();
+    })();
+  </script>
+
   <body class="test">
     <? include 'include/_toasters.php' ?>
     <nav class="navbar navbar-expand-lg
