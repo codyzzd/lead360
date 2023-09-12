126630bc69a662ceb5636b16ac16789f29471cf9
codyzzd <codyzzd@gmail.com>
2023-09-12 00:07:31 -0300

    debug email test

--- teste_email.php
+++ teste_email.php
@@ -3,6 +3,26 @@
 include 'config.php';
 $apiKey = API_KEY;
 
+$data = array(
+  "to" => array(
+    array(
+      "email" => "codyzzd@gmail.com",
+      "name" => "Bruno Gonçalves"
+    )
+  ),
+  "templateId" => 2,
+  "params" => array(
+    "PART" => "ParticipanteX",
+    "LIDER" => "LiderY",
+    "LINK" => "LinksW"
+  ),
+  "headers" => array(
+    "charset" => "iso-8859-1"
+  )
+);
+
+$json_data = json_encode($data);
+
 $ch = curl_init();
 
 curl_setopt($ch, CURLOPT_URL, 'https://api.brevo.com/v3/smtp/email');
@@ -15,22 +35,7 @@ curl_setopt(
     'content-type: application/json',
   )
 );
-curl_setopt($ch, CURLOPT_POSTFIELDS, '{
-  "to":[
-     {
-        "email":"codyzzd@gmail.com",
-        "name":"Bruno Gonçalves"
-     }
-  ],
-  "templateId":2,
-  "params":{
-     "PART":"ParticipanteX",
-     "LIDER":"LiderY",
-     "LINK":"LinksW"
-  },
-  "headers":{
-     "charset":"iso-8859-1"
-  }');
+curl_setopt($ch, CURLOPT_POSTFIELDS, $json_data);
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 
 $response = curl_exec($ch);
@@ -39,9 +44,7 @@ if (curl_errno($ch)) {
   echo 'Error: ' . curl_error($ch);
 } else {
   echo $response;
-  //echo 'ok';
 }
 
 curl_close($ch);
-
 ?>
