3d366078b75912ffc25189993f585e3e96528f24
codyzzd <codyzzd@gmail.com>
2023-09-12 23:18:42 -0300

    fix brevo cota

--- admin/brevo.php
+++ admin/brevo.php
@@ -1,20 +1,21 @@
 <?php
 //atualizar banco
-header("Access-Control-Allow-Origin: *");
+//header("Access-Control-Allow-Origin: *");
 // Conexão com o banco de dados
-require_once '../include/_dbcon.php';
-
-$dataatual = getDataAtualSaoPaulo();
-
-$sql_cota = "UPDATE brevo, data
-    SET cota = 300, data = '$dataatual'";
-$res_cota = $conn->query($sql_cota);
-
+require_once '/include/_dbcon.php';
 
 function getDataAtualSaoPaulo()
 {
   date_default_timezone_set('America/Sao_Paulo'); // Define o fuso horário para São Paulo
   return date('Y-m-d H:i:s'); // Obtém a data e hora atual formatada
 }
+$dataatual = getDataAtualSaoPaulo();
+
+$sql_cota = "UPDATE brevo, data
+    SET cota = 300, data = '$dataatual'";
+$res_cota = $conn->query($sql_cota);
+
+echo $dataatual;
+
 
 ?>
