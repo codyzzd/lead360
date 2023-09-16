d882ec0ad4454aa55165de11fc750a57315e6f10
codyzzd <codyzzd@gmail.com>
2023-09-16 17:34:24 -0300

    ajuste total menu

--- include/_nav.php
+++ include/_nav.php
@@ -12,50 +12,54 @@
            class="d-inline-block align-text-top me-2">LiderScan
     </a>
 
-    <button class="navbar-toggler"
-            type="button"
-            data-bs-toggle="collapse"
-            data-bs-target="#navbarNavAltMarkup"
-            aria-controls="navbarNavAltMarkup"
-            aria-expanded="false"
-            aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button>
-
     <div class="collapse navbar-collapse"
          id="navbarNavAltMarkup">
 
       <div class="navbar-nav">
-        <li class="nav-item"><a class="nav-link"
-             href="index.php">Avaliações</a></li>
-        <li class="nav-item"> <a class="nav-link"
-             href="participantes.php">Participantes</a></li>
-
-        <div class="d-flex">
-          <li class="nav-item dropdown">
-            <a class="nav-link dropdown-toggle"
-               href="#"
-               role="button"
-               data-bs-toggle="dropdown"
-               aria-expanded="false">
-              Conta
-            </a>
-            <ul class="dropdown-menu">
-              <li><a class="dropdown-item"
-                   href="#">Ajustes</a></li>
-              <li><a class="dropdown-item"
-                   href="../include/_a_logoff.php">Sair</a></li>
-              <!-- <li>
-              <hr class="dropdown-divider">
-            </li>
-            <li><a class="dropdown-item"
-                 href="#">Something else here</a></li>-->
-            </ul>
-          </li>
-        </div>
-
+        <li class="nav-item">
+          <a class="nav-link"
+             href="index.php">Avaliações</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link"
+             href="participantes.php">Participantes</a>
+        </li>
       </div>
 
+    </div>
+
+    <div class="d-flex">
+      <ul class="navbar-nav">
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle"
+             href="#"
+             role="button"
+             data-bs-toggle="dropdown"
+             aria-expanded="false">
+            Conta
+          </a>
+          <ul class="dropdown-menu">
+            <li>
+              <a class="dropdown-item"
+                 href="#">Ajustes</a>
+            </li>
+            <li>
+              <a class="dropdown-item"
+                 href="../include/_a_logoff.php">Sair</a>
+            </li>
+          </ul>
+        </li>
+      </ul>
+
+      <button class="navbar-toggler"
+              type="button"
+              data-bs-toggle="collapse"
+              data-bs-target="#navbarNavAltMarkup"
+              aria-controls="navbarNavAltMarkup"
+              aria-expanded="false"
+              aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
     </div>
 
   </div>
